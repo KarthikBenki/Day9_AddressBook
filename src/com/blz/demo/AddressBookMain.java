3d26a75c13cb5d379a4d095de619370e357322ec
karthikbenki <karthikmc007@gmail.com>
2022-02-16 17:50:00 +0530

    Added: Welcome message

---
+++ src/com/blz/demo/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.blz.demo;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+	System.out.println("Welcome to Address Book Program in AddressBookMain class");
+    }
+
+}
